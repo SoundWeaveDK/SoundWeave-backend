69d9801e8eff2a135c3704f53dadafab5f4713de
frede <frederik@rovi.dk>
2023-09-27 19:27:54 +0200

    Test deployment

--- src/routes/example/index.ts
+++ src/routes/example/index.ts
@@ -1,19 +1,25 @@
 import { FastifyPluginAsync, FastifyReply, FastifyRequest } from "fastify";
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 import { testInterfaceD } from "../../interfaces/testInterface";
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get("/", async function (request: FastifyRequest<{ Body: testInterfaceD }>, reply: FastifyReply) {
-    const { name, email } = request.body;
-    const user = await prisma.user.create({
-      data: {
-        email: email,
-        name: name
-      },
-    })
-    reply.send(user);
-  });
+  fastify.get(
+    "/",
+    async function (
+      request: FastifyRequest<{ Body: testInterfaceD }>,
+      reply: FastifyReply
+    ) {
+      const { name, email } = request.body;
+      const user = await prisma.user.create({
+        data: {
+          email: email,
+          name: name,
+        },
+      });
+      reply.send(user);
+    }
+  );
 };
 
 export default example;
